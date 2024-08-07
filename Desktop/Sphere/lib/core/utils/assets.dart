dc2281787601d218a21f2e534966ee75f55bd4ee
ahmedm3bed <153545608+ahmedm3bed@users.noreply.github.com>
2024-08-07 07:57:10 -0700

    Delete Desktop/Sphere directory

--- Desktop/Sphere/lib/core/utils/assets.dart
+++
@@ -1,4 +0,0 @@
-class AssetsData{
-  static const kGifLogo = 'assets/images/Sphere.gif';
-  static const kLogo = 'assets/images/Sphere.png';
-}
--- Desktop/Sphere/lib/features/auth/data/models/auth_success_model.dart
+++
@@ -1,9 +0,0 @@
-class AuthSuccessModel{
-  final String message;
-  final String token;
-
-  AuthSuccessModel({required this.message, required this.token});
-  factory AuthSuccessModel.fromJson(json){
-    return AuthSuccessModel(message: json['message'], token: json['token']);
-  }
-}
